9a24eee7c65a76c83b57a0617e8620a1ab11ac3f
cakyus <cakyus@gmail.com>
2014-08-13 23:16:31 +0700

    + application contants

--- index.php
+++ index.php
@@ -5,20 +5,24 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 date_default_timezone_set('Asia/Jakarta');
 
-include(__DIR__
-	.DIRECTORY_SEPARATOR.'vendor'
+define('APPLICATION_ROOT', __DIR__);
+define('APPLICATION_PATH', APPLICATION_ROOT.DIRECTORY_SEPARATOR.'app');
+define('APPLICATION_DATA', APPLICATION_ROOT.DIRECTORY_SEPARATOR.'data');
+define('APPLICATION_VENDOR', APPLICATION_ROOT.DIRECTORY_SEPARATOR.'vendor');
+
+include(APPLICATION_VENDOR
 	.DIRECTORY_SEPARATOR.'Nmon'
 	.DIRECTORY_SEPARATOR.'Application.php'
 	);
